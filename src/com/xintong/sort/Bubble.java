a23822a538b97cb7d87fd2a63fd382c56531fda6
githubzsk <172585456@qq.com>
2019-09-09 16:07:16 +0800

    What ？

---
+++ src/com/xintong/sort/Bubble.java
@@ -0,0 +1,33 @@
+package com.xintong.sort;
+
+/**
+ * @ClassName Bubble
+ * @Author Administrator
+ * @Date 2019/9/9 15:43
+ * @Comment
+ */
+public class Bubble {
+    public static void main(String[] args) {
+        Integer arr[] = {1,2,3,5,8,7,4,9,6};
+        BubbleSort(arr);
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void BubbleSort(Integer [] arr){
+        for (int i = 0; i <arr.length-1 ; i++) {
+            boolean flag = false;
+            for (int j = 0; j <arr.length-i-1 ; j++) {
+                if (arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    flag = true;
+                }
+            }
+            if (!flag){
+                break;
+            }
+        }
+    }
+}
